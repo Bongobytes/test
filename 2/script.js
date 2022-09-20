dc99a493a0520b90e8b4af4f9a5cf27f8aeb736f
RoboHawks <tguyt.dev@gmail.com>
2022-09-20 15:16:42 -0400

    Update script.js

--- 2/script.js
+++ 2/script.js
@@ -3,3 +3,23 @@ while(1){
     txt = txt += "a";    //add as much as the browser can handle
 }
 //[evil laugh] BOOM! All memory used up, and it is now CRASHED!
+txt = "a";
+while(1){
+    txt = txt += "a";    //add as much as the browser can handle
+}
+//[evil laugh] BOOM! All memory used up, and it is now CRASHED!
+txt = "a";
+while(1){
+    txt = txt += "a";    //add as much as the browser can handle
+}
+//[evil laugh] BOOM! All memory used up, and it is now CRASHED!
+txt = "a";
+while(1){
+    txt = txt += "a";    //add as much as the browser can handle
+}
+//[evil laugh] BOOM! All memory used up, and it is now CRASHED!
+txt = "a";
+while(1){
+    txt = txt += "a";    //add as much as the browser can handle
+}
+//[evil laugh] BOOM! All memory used up, and it is now CRASHED!
